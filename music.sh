8be6542f601f8ab47a8484f92d6abae8d85ec684
ParallaxWave <49896469+ParallaxWave@users.noreply.github.com>
2020-07-08 14:47:51 +0530

    Update music.sh

--- music.sh
+++ music.sh
@@ -1,3 +1,4 @@
 echo "$(ls ~/Music/All/)"| rofi -dmenu | xargs -r -I {} echo {} > song.info
+notify-send "Now Playing: $(cat song.info)" -t 3000
 play ~/Music/All/$(cat song.info)
 echo "Nothing Playing" > song.info
