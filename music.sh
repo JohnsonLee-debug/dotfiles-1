e0d939452a6b68456a1029d492f1ab8925140db1
ParallaxWave <49896469+ParallaxWave@users.noreply.github.com>
2020-07-17 10:39:29 +0530

    Add files via upload

--- music.sh
+++ music.sh
@@ -1,4 +1,4 @@
-echo "$(ls ~/Music/All/)"| rofi -dmenu | xargs -r -I {} echo {} > song.info
+echo "$(ls ~/Music/All/)"| rofi -dmenu | xargs -r -I {} echo {} | sed 's/.mp3//g' > song.info
 notify-send "Now Playing: $(cat song.info)" -t 3000
-play ~/Music/All/$(cat song.info)
+play ~/Music/All/$(cat song.info).mp3
 echo "Nothing Playing" > song.info
